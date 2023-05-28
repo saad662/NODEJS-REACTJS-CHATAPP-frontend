b265ea13f12a27caa248700ca5c41db79a5b2a1c
Saad Amin <52550449+saad662@users.noreply.github.com>
2023-05-28 11:31:57 -0700

    Update AuthPage.jsx

--- src/AuthPage.jsx
+++ src/AuthPage.jsx
@@ -5,7 +5,7 @@ const AuthPage = (props) => {
         e.preventDefault();
         const { value } = e.target[0];
         axios.post(
-            'http://localhost:3001/authenticate',
+            'http://nodejs-reactjs-chatapp-backend-production.up.railway.app',
             { username: value }
         )
             .then(r => props.onAuth({ ...r.data, secret: value }))
